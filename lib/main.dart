58d2ae66a3620bf91ef5632fd66e4b553b66cbdf
mrkonz <mrkonz@odds.team>
2024-02-23 17:42:02 +0700

    install firebase auth and set up firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:chat_app/screens/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -16,7 +23,7 @@ class MyApp extends StatelessWidget {
           useMaterial3: true,
           colorScheme: ColorScheme.fromSeed(
               seedColor: const Color.fromARGB(255, 63, 17, 177))),
-      home: Container(),
+      home: const AuthScreen(),
     );
   }
 }
--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+
+final _firebase = FirebaseAuth.instance;
 
 class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key});
@@ -10,6 +13,39 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  final _from = GlobalKey<FormState>();
+  var _enterEmail = '';
+  var _enterPassword = '';
+
+  var _isLogin = true;
+
+  void _submit() async {
+    final isValid = _from.currentState!.validate();
+
+    if (!isValid) {
+      return;
+    }
+    _from.currentState!.save();
+    try {
+      if (_isLogin) {
+        final userCredential = await _firebase.signInWithEmailAndPassword(
+            email: _enterEmail, password: _enterPassword);
+        print(userCredential);
+      } else {
+        final userCredential = await _firebase.createUserWithEmailAndPassword(
+            email: _enterEmail, password: _enterPassword);
+        print(userCredential);
+      }
+    } on FirebaseAuthException catch (error) {
+      if (error.code == 'email-already-in-use') {
+        // to do sumthing
+      }
+      ScaffoldMessenger.of(context).clearSnackBars();
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text(error.message ?? 'Authentication failed.')));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +53,82 @@ class _AuthScreenState extends State<AuthScreen> {
       body: Center(
         child: SingleChildScrollView(
             child: Column(
-          children: [],
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              margin: const EdgeInsets.only(
+                  top: 30, bottom: 20, left: 20, right: 20),
+              width: 200,
+              child: Image.asset('assets/images/chat.png'),
+            ),
+            Card(
+              margin: const EdgeInsets.all(20),
+              child: SingleChildScrollView(
+                  child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Form(
+                  key: _from,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      TextFormField(
+                        decoration:
+                            const InputDecoration(labelText: 'Email Address'),
+                        keyboardType: TextInputType.emailAddress,
+                        autocorrect: false,
+                        validator: (value) {
+                          if (value == null ||
+                              value.trim().isEmpty ||
+                              !value.contains('@')) {
+                            return 'Please enter valid email address';
+                          }
+                          return null;
+                        },
+                        textCapitalization: TextCapitalization.none,
+                        onSaved: (newValue) {
+                          _enterEmail = newValue!;
+                        },
+                      ),
+                      TextFormField(
+                        decoration:
+                            const InputDecoration(labelText: 'Password'),
+                        obscureText: true,
+                        validator: (value) {
+                          if (value == null || value.trim().length < 6) {
+                            return 'Password must be least 6 characters';
+                          }
+                          return null;
+                        },
+                        onSaved: (newValue) {
+                          _enterPassword = newValue!;
+                        },
+                      ),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      ElevatedButton(
+                        onPressed: _submit,
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor:
+                                Theme.of(context).colorScheme.primaryContainer),
+                        child: Text(_isLogin ? 'Login' : 'Signup'),
+                      ),
+                      TextButton(
+                        onPressed: (() {
+                          setState(() {
+                            _isLogin = !_isLogin;
+                          });
+                        }),
+                        child: Text(_isLogin
+                            ? 'Create an account'
+                            : 'I already have an account.'),
+                      ),
+                    ],
+                  ),
+                ),
+              )),
+            )
+          ],
         )),
       ),
     );
