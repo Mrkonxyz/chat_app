33e75b57597c7009beb44f0de1bb11fa7885ad0a
mrkonz <mrkonz@odds.team>
2024-02-29 16:57:55 +0700

    fireStroage

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+
+import 'package:chat_app/widgets/user_image_picker.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -16,25 +21,45 @@ class _AuthScreenState extends State<AuthScreen> {
   final _from = GlobalKey<FormState>();
   var _enterEmail = '';
   var _enterPassword = '';
+  bool _isAuthenticating = false;
+  File? _selectedImage = null;
 
   var _isLogin = true;
 
   void _submit() async {
     final isValid = _from.currentState!.validate();
 
-    if (!isValid) {
+    if (!isValid || !_isLogin && _selectedImage == null) {
       return;
     }
+
     _from.currentState!.save();
     try {
+      setState(() {
+        _isAuthenticating = true;
+      });
       if (_isLogin) {
         final userCredential = await _firebase.signInWithEmailAndPassword(
             email: _enterEmail, password: _enterPassword);
-        print(userCredential);
       } else {
         final userCredential = await _firebase.createUserWithEmailAndPassword(
             email: _enterEmail, password: _enterPassword);
-        print(userCredential);
+        final storageRef = FirebaseStorage.instance
+            .ref()
+            .child('user_images')
+            .child('${userCredential.user!.uid}.jpg');
+        await storageRef.putFile(_selectedImage!);
+
+        final imageUrl = await storageRef.getDownloadURL();
+
+        await FirebaseFirestore.instance
+            .collection('users')
+            .doc(userCredential.user!.uid)
+            .set({
+          'username': "to be done",
+          "email": _enterEmail,
+          "image_url": imageUrl
+        });
       }
     } on FirebaseAuthException catch (error) {
       if (error.code == 'email-already-in-use') {
@@ -43,6 +68,9 @@ class _AuthScreenState extends State<AuthScreen> {
       ScaffoldMessenger.of(context).clearSnackBars();
       ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(content: Text(error.message ?? 'Authentication failed.')));
+      setState(() {
+        _isAuthenticating = false;
+      });
     }
   }
 
@@ -71,6 +99,12 @@ class _AuthScreenState extends State<AuthScreen> {
                   child: Column(
                     mainAxisSize: MainAxisSize.min,
                     children: [
+                      if (!_isLogin)
+                        UserImagePicker(
+                          onPickImage: (pickImage) {
+                            _selectedImage = pickImage;
+                          },
+                        ),
                       TextFormField(
                         decoration:
                             const InputDecoration(labelText: 'Email Address'),
@@ -106,23 +140,27 @@ class _AuthScreenState extends State<AuthScreen> {
                       const SizedBox(
                         height: 12,
                       ),
-                      ElevatedButton(
-                        onPressed: _submit,
-                        style: ElevatedButton.styleFrom(
-                            backgroundColor:
-                                Theme.of(context).colorScheme.primaryContainer),
-                        child: Text(_isLogin ? 'Login' : 'Signup'),
-                      ),
-                      TextButton(
-                        onPressed: (() {
-                          setState(() {
-                            _isLogin = !_isLogin;
-                          });
-                        }),
-                        child: Text(_isLogin
-                            ? 'Create an account'
-                            : 'I already have an account.'),
-                      ),
+                      if (_isAuthenticating) const CircularProgressIndicator(),
+                      if (!_isAuthenticating)
+                        ElevatedButton(
+                          onPressed: _submit,
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Theme.of(context)
+                                  .colorScheme
+                                  .primaryContainer),
+                          child: Text(_isLogin ? 'Login' : 'Signup'),
+                        ),
+                      if (!_isAuthenticating)
+                        TextButton(
+                          onPressed: (() {
+                            setState(() {
+                              _isLogin = !_isLogin;
+                            });
+                          }),
+                          child: Text(_isLogin
+                              ? 'Create an account'
+                              : 'I already have an account.'),
+                        ),
                     ],
                   ),
                 ),
--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -9,6 +10,15 @@ class ChatScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+            icon: const Icon(Icons.exit_to_app),
+            color: Theme.of(context).colorScheme.primary,
+          )
+        ],
         title: const Text("Flutter Chat App"),
       ),
       body: const Center(
